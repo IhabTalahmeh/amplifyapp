260a7852cb114b67fe257dabe6f461deef02e465
DESKTOP-SKNORER\Training <ihab.talahma@gmail.com>
2022-10-08 01:05:30 +0300

    add amplify

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-        Hello test Amplify
+        Hello Mariana!
         </p>
         <a
           className="App-link"
